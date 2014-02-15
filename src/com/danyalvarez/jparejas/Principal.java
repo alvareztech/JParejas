213e03615dd1ffa1b76b4ae016edd6c18406a68b
Daniel Alvarez <i@danyalvarez.com>
2014-02-15 10:24:21 -0400

    Initial commit

---
+++ src/com/danyalvarez/jparejas/Principal.java
@@ -0,0 +1,12 @@
+/**
+ * @author Daniel Alvarez
+ * @date 16 de octubre de 2009
+ */
+
+package com.danyalvarez.jparejas;
+
+public class Principal {
+    public static void main(String[] a3d) {
+        new Parejas();
+    }
+}
